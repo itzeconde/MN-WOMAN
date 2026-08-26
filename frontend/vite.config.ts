0b3c046dc3ae7df931f5b7c93df6575e761495fd
Itzel conde <chelconde46@gmail.com>
2026-08-26 13:23:49 -0600

    Vite configurado

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    Sitemap({
+      hostname: 'https://mn-woman.vercel.app',
+      dynamicRoutes: [
+        '/',
+        '/cursos',
+        '/linea911',
+        '/articulos',
+        '/sobre-nosotros',
+        '/privacidad',
+      ],
+    }),
+  ],
 })
